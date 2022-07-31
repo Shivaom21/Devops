6e128aa1582aa4640eb67a60e0c8577095a4a997
shivaom21 <joelsondefarias@gmail.com>
2022-07-31 19:02:35 -0300

    confirmado

--- informacoes-do-sistema.sh
+++ informacoes-do-sistema.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
-
-# Sistema - Script que mostra informações sobre o sistema
+#
+# Sistema - Script que mostra informaï¿½ï¿½es sobre o sistema
+#
 # Autor: Joelson de Souza
-
-# Pede uma confirmação do usuário antes de executar
+# Pede uma confirmaï¿½ï¿½o do usuï¿½rio antes de executar
 echo "Vou buscar os dados do sistema. Posso continuar ? [sn]"
 read RESPOSTA
-
+#
 # Se ele digitou 'n', vamos interromper o script
 test "$RESPOSTA" = "n" && exit
-
+#
 # O date mostra a data e hora correntes
-echo "Data e Horário:"
+echo "Data e Horï¿½rio:"
 date
 echo
-
-# O df mostra as partições e quanto cada uma ocupa no disco
+#
+# O df mostra as partiï¿½ï¿½es e quanto cada uma ocupa no disco
 echo "Uso do disco:"
 df
 echo
-
-# O w mostra os usuários que estão conectados nesta maquina
-echo "Usuários conectados:"
+#
+# O w mostra os usuï¿½rios que estï¿½o conectados nesta maquina
+echo "Usuï¿½rios conectados:"
 w
+echo
